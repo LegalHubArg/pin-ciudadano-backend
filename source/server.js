3529d89b184b755ee7b0af5d5b4f424342c15957
Nahuel <nahuel@legalhub.la>
2024-06-24 10:49:54 -0300

    Commit de prueba SSH

--- source/server.js
+++ source/server.js
@@ -1,89 +1,84 @@
-const express = require('express');
-require('dotenv').config()
-const app = express()
-const cors = require("cors")
-const bodyParser = require('body-parser');
-const http = require('http');
-const routes = require('./routes/rutas')
-const morgan = require('morgan')
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const http = require("http");
+const routes = require("./routes/rutas");
+const morgan = require("morgan");
 
 const port = process.env.API_PORT;
 //app.listen(process.env.API_PORT, () => { console.log('Listening on 9000') })
 
-const apiversion = '/api/' + process.env.API_VERSION;
+const apiversion = "/api/" + process.env.API_VERSION;
 
-const whitelist = process.env.API_WHITELIST.split(',')
+// const whitelist = process.env.API_WHITELIST.split(',')
 const corsOptions = {
   origin: function (origin, callback) {
     if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
+      callback(null, true);
       console.log("CORS OK FROM: ", origin);
     } else {
-      callback(new Error("Not allowed by CORS"))
+      callback(new Error("Not allowed by CORS"));
       console.log("CORS ERROR FROM: ", origin);
     }
   },
   credentials: true,
-}
-app.use(cors(corsOptions))
+};
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 
 //Logger
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
 //HEALTH
 async function pinrun(req, res) {
-  res.status(200)
-  res.send(JSON.stringify({ status: 'OK' }))
+  res.status(200);
+  res.send(JSON.stringify({ status: "OK" }));
   res.end();
 }
-app.get(apiversion +'/pin-run', pinrun);
+app.get(apiversion + "/pin-run", pinrun);
 //Rutas
-app.use(apiversion, routes)
+app.use(apiversion, routes);
 
-app.use('*', (req, res) => {
-    res.status(404)
-    //res.send(JSON.stringify({ mensaje: 'PIN: Ruta inválida.', error: 'Petición no permitida.' }))
-    res.end();
-  });
+app.use("*", (req, res) => {
+  res.status(404);
+  //res.send(JSON.stringify({ mensaje: 'PIN: Ruta inválida.', error: 'Petición no permitida.' }))
+  res.end();
+});
 
 const server = http.createServer(app);
 
-server.listen(port)
-server.on('error', onError);
-server.on('listening', onListening);
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
 
 function onError(error) {
-    if (error.syscall !== 'listen') {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
       throw error;
-    }
-  
-    const bind = typeof port === 'string'
-      ? 'Pipe ' + port
-      : 'Port ' + port;
-  
-    // handle specific listen errors with friendly messages
-    switch (error.code) {
-      case 'EACCES':
-        console.error(bind + ' requires elevated privileges');
-        process.exit(1);
-        break;
-      case 'EADDRINUSE':
-        console.error(bind + ' is already in use');
-        process.exit(1);
-        break;
-      default:
-        throw error;
-    }
   }
-  
-  
-  function onListening() {
-    const addr = server.address();
-    const bind = typeof addr === 'string'
-      ? 'pipe ' + addr
-      : 'port ' + addr.port;
-  
-    console.log('Frontoffice Backend Running in ' + bind + '...')
-  }
+}
+
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+
+  console.log("Frontoffice Backend Running in " + bind + "...");
+}
